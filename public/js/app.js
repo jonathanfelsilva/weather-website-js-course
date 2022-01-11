eaa1e9c7a202d0188a719ebcc8ac28728c059245
Jonathan Silva <jonathan.silva@ideagri.com.br>
2022-01-11 12:34:00 -0300

    Removendo console.log desnecessário

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Message.")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const resultMessage = document.querySelector('#result-message')
